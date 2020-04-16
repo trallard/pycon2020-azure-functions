c399267f8d26c766df5ef466b446e9febd03d6c5
Tania Allard <taniar.allard@gmail.com>
2020-04-16 10:59:48 +0100

    :see_no_evil: Update gitignore

--- noxfile.py
+++ noxfile.py
@@ -3,13 +3,25 @@ import nox
 
 @nox.session(python=False)
 def docs(session):
+    """Build the documentation, note this uses Poetry"""
     session.chdir("docs")
     session.run("rm", "-rf", "_build/", external=True)
     session.run("poetry", "shell")
     session.run("poetry", "install")
-    sphinx_args = ["-W", ".", "_build/html"]
+    sphinx_args = ["-b", "html", "-W", "-d", "_build/doctrees", ".", "_build/html"]
 
     if "serve" in session.posargs:
-        session.run("sphinx-autobuild", *sphinx_args)
+        sphinx_cmd = "sphinx-autobuild"
+        sphinx_args.insert(0, "--open-browser")
     else:
-        session.run("sphinx-build", *sphinx_args)
+        sphinx_cmd = "sphinx-build"
+
+     session.run(sphinx_cmd, *sphinx_args)
+
+@nox.session(python="3.8")
+def blacken(session):
+    """Run black code formater."""
+    session.install("black==19.3b0", "isort==4.3.21")
+    files = ["**/*.py"]
+    session.run("black", *files)
+    session.run("isort", "--recursive", *files)
